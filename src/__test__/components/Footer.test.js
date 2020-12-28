674c25327c58ddb568848b2241759978e6c5aaea
Victor <victorlanda.dev@gmail.com>
2020-12-27 21:00:05 -0600

    test actions

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -9,7 +9,7 @@ describe('<Footer/>', () => {
     expect(footer.length).toEqual(1);
   });
   test('Render del título', () => {
-    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+    expect(footer.find('.Footer-title').text()).toEqual('Houdini Store');
   });
 });
 
